a9566dc8d956bb41eed10d291dd6d24c458cc4cf
James Dalton <tarpdalton@users.noreply.github.com>
2020-08-31 13:32:13 -0700

    pass state

--- lib/neutron/pulsar_consumer_callback.ex
+++ lib/neutron/pulsar_consumer_callback.ex
@@ -1,4 +1,4 @@
 defmodule Neutron.PulsarConsumerCallback do
   @moduledoc "callback for consumers to implement for handling a message and whether to ack it"
-  @callback handle_message(String.t()) :: :ack | :ack_all | :nack
+  @callback handle_message(String.t(), any()) :: :ack | :ack_all | :nack
 end
