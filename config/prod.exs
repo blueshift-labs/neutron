1f261001bf6294c981b52ae31eede7b8d22104ec
Esteban Zapata Rojas <esteban.zapata@avlos.ai>
2024-02-08 17:31:55 -0500

    config: Use `:warning` level instead of deprecated `:warn`.

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,7 @@ import Config
 
 # Do not print debug/info messages in production
 config :logger,
-  level: :warn,
+  level: :warning,
   compile_time_purge_matching: [
-    [level_lower_than: :warn]
+    [level_lower_than: :warning]
   ]
